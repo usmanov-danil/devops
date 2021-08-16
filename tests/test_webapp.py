affc6e081c6b3f4766ebcbd18d731a71c77a3586
Danil Usmanov <usmanovdanilr@gmail.com>
2021-08-16 23:39:42 +0300

    Add tests, Update requirements

---
+++ tests/test_webapp.py
@@ -0,0 +1,15 @@
+from app_python.main import app
+
+
+class TestApp():
+    def test_correct_url(self):
+        client = app.test_client(self)
+        response = client.get('/')
+
+        assert response.status_code == 200
+
+    def test_incorrect_url(self):
+        client = app.test_client(self)
+        response = client.get('/unknow_endpoint/')
+
+        assert response.status_code == 404
